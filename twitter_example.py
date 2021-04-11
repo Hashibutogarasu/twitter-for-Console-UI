aa2413099f8dcd7faab933cc0c37eb0ec9d8f233
カラス <71928247+Hashibutogarasu@users.noreply.github.com>
2021-04-11 17:16:37 +0900

    Update twitter_example.py

--- twitter_example.py
+++ twitter_example.py
@@ -1,109 +1,147 @@
-print('Twitter for CUIを起動しています...')
+from pip._internal import main as _main
+import importlib
+
+def _import(name, module, ver=None):
+    try:
+        globals()[name] = importlib.import_module(module)
+    except ImportError:
+        try:
+            if ver is None:
+                _main(['install', module])
+            else:
+                _main(['install', '{}=={}'.format(module, ver)])
+            globals()[name] = importlib.import_module(module)
+        except:
+            print("can't import: {}".format(module))
+import os
+print("[INFO]osライブラリをインポートしました。")
+
 import sys
-import tweepy
-import webbrowser
-import urllib
-from selenium import webdriver
+print("[INFO]sysライブラリをインポートしました。")
+
 import time
-print('インポートが完了しました。')
+print("[INFO]timeライブラリをインポートしました。")
 
-API_KEY = '**********'  
-API_KEY_SECRET = '*********'
+try:
+    import tweepy
+    print("[INFO]tweepyライブラリをインポートしました。")
+except:
+    print("[WARN]お使いのPythonライブラリにtweepyライブラリがインストールされていません。")
+    imp = input("[INFO]tweepyをインストールしますか？\nyでインストール\n")
+    if (imp == "y"):
+        _import('pd','tweepy', '3.10.0')
+        print(pd)
+        print("インストールが完了しました。\n一旦終了します。")
+        time.sleep(1)
+        sys.exit()
+    else:
+        print("キャンセルしました。")   
+        time.sleep(1)
+        sys.exit()
 
-def get_oauth_token(url:str)->str:
-    querys = urllib.parse.urlparse(url).query
-    querys_dict = urllib.parse.parse_qs(querys)
-    return querys_dict["oauth_token"][0]
+import webbrowser
+print("[INFO]webbrowserライブラリをインポートしました。")
+
+import urllib
+print("[INFO]urllibライブラリをインポートしました。")
+
+#キーなど
+API_KEY = ''
+API_KEY_SECRET = ''
+access_token = ''
+access_token_secret = ''
+
 
 if __name__ == '__main__':
 
     auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
-
-    try:
-        redirect_url = auth.get_authorization_url()
-    except tweepy.TweepError:
-        print( "リクエストトークンの取得に失敗しました。")
-
-
-    oauth_token = get_oauth_token(redirect_url)
-    auth.request_token['oauth_token'] = oauth_token
-
-    webbrowser.open(redirect_url)
-
-    verifier = input("ブラウザに表示されているPINコードを入力してください。:")
-    verifier_none = verifier.strip()
-    #PINコードに含まれている空白を削除
-
-    auth.request_token['oauth_token_secret'] = verifier_none
-
-    if len(verifier_none) == 0:
-        print('PINコードが未入力です。')
-        sys.exit()
-        #未入力の場合強制終了する
-
-    try:
-        auth.get_access_token(verifier_none)
-    except tweepy.TweepError:
-        print("アクセストークンの入手に失敗しました。")
-        sys.exit()
-
-    with open("auth_info.txt",mode="w") as file:
-        text = "key:{}\nsecret:{}".format(auth.access_token,auth.access_token_secret)
-        file.write(text)
+    auth.set_access_token(access_token, access_token_secret)
     
+    api = tweepy.API(
+        auth,
+        wait_on_rate_limit=True,
+        wait_on_rate_limit_notify=True
+    )
+    
+    try:
+        me = api.me()
+        print("\n認証が完了しました。\n")
+        print('ようこそ  {}  @{}\n'.format(me.name,me.screen_name))
+    except:
+        print("ユーザー名を取得できませんでした。")
+        print("キーがセットされていないまたはアカウントが使用不可になっている可能性があります。")
+        print("Twitter for CUIを終了します。")
+        time.sleep(1)
+        sys.exit()
+        
+    while True:
+        twitter_cui_command=input("Twitter for CUI:")
+        if (twitter_cui_command == "Tweet") :
+            select=input("画像を添付しますか？y/n\nDefault→n\nlogoutコマンドで終了できます。\n")
+            if (select == "y") :
+                tweet_content_with_image=input("ツイートする内容を入力してください。\ncancelコマンドで終了ができます。")
+                if (tweet_content_with_image == "cancel") :
+                    print("キャンセルしました。")
+                else:
+                    try:
+                        file_names='./images/tweet.png'
+                        api.update_with_media(filename=file_names,status=tweet_content_with_image) 
+                        print("imagesフォルダ内にあるtweet.pngを添付してツイートを送信しました。")
+                    except:    
+                        print("tweet.pngが見つかりませんでした。")
+                        api.update_status(status=tweet_content_with_image)
+                        print("画像無しでツイートを送信しました。") 
 
-    if auth.access_token is None :
-        if auth.access_token_secret is None :
-            print('PINコードが間違っています。')
-            print('Twitter for CUIを終了します。')
-            time.sleep(2)
-            sys.exit()
-
-    print("認証が完了しました。")
-
-auth.set_access_token(auth.access_token, auth.access_token_secret)
-
-api = tweepy.API(
-    auth,
-    wait_on_rate_limit=True,
-    wait_on_rate_limit_notify=True
-)
-
-me = api.me()
-print('ようこそ',me.name,me.screen_name)
-
-while True:
-    twitter_cui_command=input("コマンドを入力...\nhelpコマンドでコマンドの一覧を表示できます。\n")
-    if (twitter_cui_command == "Tweet") :
-        select=input("画像を添付しますか？y/n\nDefault→n\nlogoutコマンドで終了できます。\n")
-        if (select == "y") :
-            tweet_content_with_image=input("ツイートする内容を入力してください。\n何も入力しないでエンターキーを押すと画像のみを送信できます。\nまた、logoutコマンドで終了もできます。")
-            if (tweet_content_with_image == "logout") :
-                break
-            file_names='./images/tweet.png'
-            api.update_with_media(filename=file_names,status=tweet_content_with_image) 
-            print("imagesフォルダ内にあるtweet.pngを添付してツイートを送信しました。")
-        elif (select == "n") :
-            tweet_content=input("ツイートする内容を入力してください。")
-            api.update_status(tweet_content)
-            print("ツイートを送信しました。")
-        elif (select == "logout" ) :
+            elif (select == "n") :
+                tweet_content=input("ツイートする内容を入力してください。")
+                api.update_status(tweet_content)
+                print("ツイートを送信しました。")
+            elif (select == "cancel" ) :
+                print("キャンセルしました。")
+            else :
+                print("キャンセルしました。")
+        elif (twitter_cui_command == "timeline") :
+            public_tweets = api.home_timeline()
+            for tweet in public_tweets:
+                print('-------------------------')
+                print(tweet.text)
+        elif (twitter_cui_command == "search") :
+            api = tweepy.API(auth)
+            search_word = input()
+            for tweet in tweepy.Cursor(api.search, q=search_word).items(5):
+                print("\n{}\n".format(tweet.user.name))
+                print(tweet.text,"\n")
+        elif (twitter_cui_command == "login as"):
+            me = api.me()
+            print("\nログイン中のアカウント:{}\n".format(me.name,me.screen_name))
+        elif (twitter_cui_command == "about me"):
+            me = api.me()
+            print("\nアカウント名:{}\n".format(me.name))
+            print("ユーザーID:{}\n".format(me.screen_name))
+            print("概要:{}\n".format(me.description))
+            print("アイコンのURL:{}\n".format(me.profile_image_url_https))
+        
+        elif (twitter_cui_command == "release note"):
+            print("\nTwitter for CUI v1.2.0\n")
+            print("変更点その１:ライブラリまたはモジュールをインポートするとき、対象がインストールされていない場合、インストールできるようにしました。\nちなみにインストールをしたとき再起動する必要があります。")
+            print("変更点その２:画像付きでツイートをする時、フォルダが存在しない、または対象の画像が存在しない場合は画像なしでツイートできるようにしました。")
+            print("変更点その３:APIキーなどが入力されていない場合、正常に終了するようにしました。")
+            print("変更点その４:about meコマンドを追加しました。ログイン中のアカウントの詳細を表示できます。")
+            print("変更点その５:release noteコマンドを追加しました。現在のバージョンと変更点を表示できます。")
+            print("変更点その６:今更ですがPINログイン機能を廃止し、アクセストークンとアクセストークンシークレットを入力して認証するようにしました。\nいちいちログイン認証するの面倒くさいし（）")
+        elif (twitter_cui_command == "help") :
+            print("")
+            print("*"*50)
+            print("\nTwitter for CUI Help\n")
+            print("Tweet:ツイートを送信するコマンド。\n")
+            print("login as:ログイン中のアカウントを表示するコマンド。\n")
+            print("about me:ログイン中のアカウントの詳細を表示するコマンド。\n")
+            print("release note:リリースノートを表示するコマンド")
+            print("logout:Twitter for CUIを終了するコマンド。\n")
+            print("*"*50)
+            print("")
+        elif (twitter_cui_command == "logout") :
+            print('Twitter for Pythonを終了します。')
             break
         else :
-            tweet_content_with_image=input("ツイートする内容を入力してください。")
-            file_names='./images/tweet.png'
-            api.update_with_media(filename=file_names,status=tweet_content_with_image) 
-            print("imagesフォルダ内にあるtweet.pngを添付してツイートを送信しました。")
-    elif (twitter_cui_command == "login as"):
-        me = api.me()
-        print("ログイン中のアカウント=>",me.name,me.screen_name)
-    elif (twitter_cui_command == "help") :
-        print("\nTwitter Python Help\n")
-        print("Tweet=>ツイートを送信するコマンド。\n")
-        print("login as=>ログイン中のアカウントを表示するコマンド。\n")
-        print("logout=>Twitter for CUIを終了するコマンド。\n")
-    elif (twitter_cui_command == "logout") :
-        print('Twitter for CUIを終了します。')
-        break
-    else :
-        print("コマンドを入力してください。\n入力したコマンド","「",twitter_cui_command,"」は存在しません。")
+            print("コマンドを入力してください。入力したコマンド:{} は存在しません。".format(twitter_cui_command))
