e0aa026f7d1f5be8d32bfeb28d26413bf89144c0
カラス <71928247+Hashibutogarasu@users.noreply.github.com>
2021-04-20 17:26:19 +0900

    Update twitter_example.py

--- twitter_example.py
+++ twitter_example.py
@@ -1,147 +1,95 @@
-from pip._internal import main as _main
-import importlib
+def main():
 
-def _import(name, module, ver=None):
+    def nowtime():
+        now = datetime.datetime.now()
+        year = now.year
+        month = now.month
+        day = now.day
+        hour = now.hour
+        minute = now.minute
+        second = now.second
+
+        nowtime = f"{year}/{month}/{day}/{hour}:{minute}"
+
+        return  nowtime
+
     try:
-        globals()[name] = importlib.import_module(module)
-    except ImportError:
-        try:
-            if ver is None:
-                _main(['install', module])
+        ak = '' #api key
+        aks = '' #api key secret
+        at = '' #access token
+        ats = '' #access token secret
+        auth = tweepy.OAuthHandler(ak,aks)
+        auth.set_access_token(at,ats)
+        api = tweepy.API(auth)
+        me = api.me()
+        print(nowtime())
+        print("Logged on")
+        print(f"name:{me.name}\ndescription:{me.description}\nurl:{me.url}\nlocation:{me.location}")
+    except:
+        print("404 Not found")
+        sys.exit()
+    while True:
+        cmd = input("Enter command:")
+        if cmd == "update profile": #change user name
+            yn = input("Are you sure to change account name?")
+            if yn == "y":
+                accountname = input("Enter your new account name:")
+                print(f"before:\n{me.name}")
+                print(f"after:\n{accountname}")
+                ynsure = input(f"Are you sure?\ny/n:")
+                if ynsure == "y":
+                    api.update_profile(accountname)
+                    print("Changed your profile")
+                    api.update_status(f"Twitter for Console UI info\nChanged account name:\n{accountname}")
+                elif ynsure == "n":
+                    print("Canceled")
+                else:
+                    print("Canceled")
+            elif yn == "n":
+                print("Canceled")
             else:
-                _main(['install', '{}=={}'.format(module, ver)])
-            globals()[name] = importlib.import_module(module)
-        except:
-            print("can't import: {}".format(module))
-import os
-print("[INFO]osライブラリをインポートしました。")
-
-import sys
-print("[INFO]sysライブラリをインポートしました。")
-
-import time
-print("[INFO]timeライブラリをインポートしました。")
-
-try:
-    import tweepy
-    print("[INFO]tweepyライブラリをインポートしました。")
-except:
-    print("[WARN]お使いのPythonライブラリにtweepyライブラリがインストールされていません。")
-    imp = input("[INFO]tweepyをインストールしますか？\nyでインストール\n")
-    if (imp == "y"):
-        _import('pd','tweepy', '3.10.0')
-        print(pd)
-        print("インストールが完了しました。\n一旦終了します。")
-        time.sleep(1)
-        sys.exit()
-    else:
-        print("キャンセルしました。")   
-        time.sleep(1)
-        sys.exit()
-
-import webbrowser
-print("[INFO]webbrowserライブラリをインポートしました。")
-
-import urllib
-print("[INFO]urllibライブラリをインポートしました。")
-
-#キーなど
-API_KEY = ''
-API_KEY_SECRET = ''
-access_token = ''
-access_token_secret = ''
-
+                print("Canceled")
+        elif cmd == "tweet": #tweet
+            tweettext = input("Enter your tweet content:").split('/newline/')
+            imagepath = input("Enter image path:")
+            if(os.path.exists(imagepath)):
+                try:
+                    api.update_with_media(status = '\n'.join(tweettext), filename = imagepath)
+                    print("Tweet sended")
+                except tweepy.TweepError as e:
+                    print(f"Coundn't send Tweet\nReason:{e}")
+            else:
+                try:
+                    api.update_status('\n'.join(tweettext))
+                    print("Tweet sended")
+                except tweepy.TweepError as e:
+                    print(f"Coundn't send Tweet\nReason:{e}")
+        elif cmd == "notice": #notice
+            timeline=api.mentions_timeline(count=38)
+            for status in timeline:
+                status_id=status.id
+                status_text = status.text
+                print(status.author.name + " @" + status.author.screen_name + "\n" + "返信先:" + status_text + "\n")
+        elif cmd == "close":
+            break
+            sys.exit()
+        elif cmd == "os version":
+            print(platform.platform())
 
 if __name__ == '__main__':
-
-    auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
-    auth.set_access_token(access_token, access_token_secret)
-    
-    api = tweepy.API(
-        auth,
-        wait_on_rate_limit=True,
-        wait_on_rate_limit_notify=True
-    )
-    
     try:
-        me = api.me()
-        print("\n認証が完了しました。\n")
-        print('ようこそ  {}  @{}\n'.format(me.name,me.screen_name))
+        import sys
+        import os
+        import platform
+        import pathlib
+        import traceback
+        import datetime
+        import calendar
+        import tweepy
+        main()
     except:
-        print("ユーザー名を取得できませんでした。")
-        print("キーがセットされていないまたはアカウントが使用不可になっている可能性があります。")
-        print("Twitter for CUIを終了します。")
-        time.sleep(1)
-        sys.exit()
-        
-    while True:
-        twitter_cui_command=input("Twitter for CUI:")
-        if (twitter_cui_command == "Tweet") :
-            select=input("画像を添付しますか？y/n\nDefault→n\nlogoutコマンドで終了できます。\n")
-            if (select == "y") :
-                tweet_content_with_image=input("ツイートする内容を入力してください。\ncancelコマンドで終了ができます。")
-                if (tweet_content_with_image == "cancel") :
-                    print("キャンセルしました。")
-                else:
-                    try:
-                        file_names='./images/tweet.png'
-                        api.update_with_media(filename=file_names,status=tweet_content_with_image) 
-                        print("imagesフォルダ内にあるtweet.pngを添付してツイートを送信しました。")
-                    except:    
-                        print("tweet.pngが見つかりませんでした。")
-                        api.update_status(status=tweet_content_with_image)
-                        print("画像無しでツイートを送信しました。") 
-
-            elif (select == "n") :
-                tweet_content=input("ツイートする内容を入力してください。")
-                api.update_status(tweet_content)
-                print("ツイートを送信しました。")
-            elif (select == "cancel" ) :
-                print("キャンセルしました。")
-            else :
-                print("キャンセルしました。")
-        elif (twitter_cui_command == "timeline") :
-            public_tweets = api.home_timeline()
-            for tweet in public_tweets:
-                print('-------------------------')
-                print(tweet.text)
-        elif (twitter_cui_command == "search") :
-            api = tweepy.API(auth)
-            search_word = input()
-            for tweet in tweepy.Cursor(api.search, q=search_word).items(5):
-                print("\n{}\n".format(tweet.user.name))
-                print(tweet.text,"\n")
-        elif (twitter_cui_command == "login as"):
-            me = api.me()
-            print("\nログイン中のアカウント:{}\n".format(me.name,me.screen_name))
-        elif (twitter_cui_command == "about me"):
-            me = api.me()
-            print("\nアカウント名:{}\n".format(me.name))
-            print("ユーザーID:{}\n".format(me.screen_name))
-            print("概要:{}\n".format(me.description))
-            print("アイコンのURL:{}\n".format(me.profile_image_url_https))
-        
-        elif (twitter_cui_command == "release note"):
-            print("\nTwitter for CUI v1.2.0\n")
-            print("変更点その１:ライブラリまたはモジュールをインポートするとき、対象がインストールされていない場合、インストールできるようにしました。\nちなみにインストールをしたとき再起動する必要があります。")
-            print("変更点その２:画像付きでツイートをする時、フォルダが存在しない、または対象の画像が存在しない場合は画像なしでツイートできるようにしました。")
-            print("変更点その３:APIキーなどが入力されていない場合、正常に終了するようにしました。")
-            print("変更点その４:about meコマンドを追加しました。ログイン中のアカウントの詳細を表示できます。")
-            print("変更点その５:release noteコマンドを追加しました。現在のバージョンと変更点を表示できます。")
-            print("変更点その６:今更ですがPINログイン機能を廃止し、アクセストークンとアクセストークンシークレットを入力して認証するようにしました。\nいちいちログイン認証するの面倒くさいし（）\n")
-        elif (twitter_cui_command == "help") :
-            print("")
-            print("*"*50)
-            print("\nTwitter for CUI Help\n")
-            print("Tweet:ツイートを送信するコマンド。\n")
-            print("login as:ログイン中のアカウントを表示するコマンド。\n")
-            print("about me:ログイン中のアカウントの詳細を表示するコマンド。\n")
-            print("release note:リリースノートを表示するコマンド")
-            print("logout:Twitter for CUIを終了するコマンド。\n")
-            print("*"*50)
-            print("")
-        elif (twitter_cui_command == "logout") :
-            print('Twitter for Pythonを終了します。')
-            break
-        else :
-            print("コマンドを入力してください。入力したコマンド:{} は存在しません。".format(twitter_cui_command))
+        traceback.print_exc()
+        while True:
+            print(f"起動に失敗しました")
+            kakuninn = input("終了するにはEnterキーを押してください...")
+            sys.exit()
