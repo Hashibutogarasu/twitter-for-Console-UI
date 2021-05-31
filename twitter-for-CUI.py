64a8d40d92765a9bd753cd1c0edcfedd941d093b
カラス <71928247+Hashibutogarasu@users.noreply.github.com>
2021-05-31 20:59:07 +0900

    Update twitter-for-CUI.py

--- twitter-for-CUI.py
+++ twitter-for-CUI.py
@@ -18,19 +18,25 @@ def twchelp(api):
 	content = ("twitter-for-CUI help\ntimeline:タイムラインを表示出来ます。\ntweet:ツイートが出来ます。\nretweet:リツイートが出来ます。\nfavorite:いいねが出来ます。\nfollow:フォローが出来ます。\nunfollow:フォロー解除が出来ます。\nuser_info:ユーザーの詳細を確認できます。\n更なる詳細は\nhttps://github.com/Hashibutogarasu/twitter-for-Console-UI\nで確認できます。")
 	print(content)
 
+def close(api):
+    sys.exit()
+
 
 COMMANDS = {
     'timeline': tw.timeline,
     'TL': tw.timeline,
     'tweet': tw.tweet,
-    'retweet': tw.retweet,
+    'destroytweet':tw.tweetdestroy,
     'favorite': tw.favorite,
+    'retweet': tw.retweet,
     'follow':tw.follow,
     'unfollow':tw.unfollow,
     'user_info':tw.profile,
+    'user_timeline':tw.user_timeline,
+    'search':tw.search,
     'help':twchelp,
-    'destroytweet':tw.tweetdestroy,
-    'user_timeline':tw.user_timeline
+    'close':close
+
 }
 
 def main():
@@ -41,4 +47,7 @@ def main():
             COMMANDS[command](api)
 
 if __name__ == "__main__":
-	main()
+    try:
+	    main()
+    except KeyboardInterrupt:
+        print("が押されました。")
