91b831b6b56d6d89e69f90bd948d0daf2bb6f645
カラス <71928247+Hashibutogarasu@users.noreply.github.com>
2021-05-31 21:01:10 +0900

    Update config.py

--- twitterfunc/config.py
+++ twitterfunc/config.py
@@ -1,4 +1,4 @@
-ck = "" #Twitter api key
-cs = "" #Twitter api key secret
-at = "" #Twitter access token
-ats = "" #Twitter access token secret
+ck = "******" #Twitter api key
+cs = "*******" #Twitter api key secret
+at = "*******" #Twitter access token
+ats = "********" #Twitter access token secret
